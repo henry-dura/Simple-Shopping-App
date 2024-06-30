041e56ca833251129109ee044c8b8b9027f65578
Henry <henryikemefuna@gmail.com>
2024-06-30 14:26:07 +0100

    Get initial setup and add product model

---
+++ lib/product_model.dart
@@ -0,0 +1,9 @@
+
+class ProductModel {
+  final int id;
+  final String title;
+  final String image;
+  final double price;
+
+  ProductModel( {required this.id, required this.title, required this.price,required this.image,});
+}
